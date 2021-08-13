fa9168216e2c398dafb594fb53c6d21514aec427
iaconom821 <iaconom821@gmail.com>
2021-08-13 10:59:17 -0400

    done

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,5 +1,14 @@
 function findShortestStringRecursive(arr) {
   // type your code here
+  if(arr.length === 1){
+    return arr[0]
+  } else {
+    if(arr[0].length <= arr[1].length){
+      arr[1] = arr[0]
+    }
+    arr.shift()
+    return findShortestStringRecursive(arr)
+  }
 }
 
 if (require.main === module) {
@@ -22,3 +31,4 @@ module.exports = findShortestStringRecursive;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// my base case is when the array's length is 1 I am returning the value of that element. In my recursive case I am checking the length of the second element agains the length of the first element and then setting the second element to the shorter of the two strings, or the first string if they are equavilent lengths. Then my recursive call I am calling returning the function call again with the first element shifted off. 
